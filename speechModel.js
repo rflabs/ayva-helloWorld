7100fbd97dc08ea0bc2dca3089ee8c0968320c27
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-02-19 11:59:20 -0500

    test

--- speechModel.js
+++ speechModel.js
@@ -1,13 +1,17 @@
 var Welcome = {
     name: 'welcome',
     utterances: [],
+    events:[
+        "Welcome"
+    ]
 }
 
 var Name = {
     name: 'name',
     utterances: [
         "My name is kevin"
-    ]
+    ],
+    events:[]
 }
 
 var speechModel = {
