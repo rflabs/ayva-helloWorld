09ff9415c4b2dc84038887d45fb9e339cead5db5
gberk <gberkowitz@logmein.com>
2018-02-20 13:43:10 -0500

    Start script

--- Errors/index.js
+++ Errors/index.js
@@ -1,9 +1,10 @@
 var InactiveIntentHandler = function(Context){
+    console.log(Context)
     Context.assistant.say("Sorry, that intent is not active now").finish()
 }
 
 
 module.exports = {
-    "InactiveIntentError": InactiveIntentHandler
+    "InactiveIntentError": InactiveIntentHandler,
 }
 
