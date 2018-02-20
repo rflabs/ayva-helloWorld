f30e2c1c7afea6746a02a4f34cbe4369fe2b4cea
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-02-19 19:24:41 -0500

    testing...

--- states.js
+++ states.js
@@ -1,7 +1,17 @@
 var States = {
     
     'welcome': {
-        activeIntents: ['input.welcome'],
+        activeIntents: ['welcome','name'],
+        middleware: ['logInput']
+    },
+
+    'newUser': {
+        activeIntents: ['welcome','name'],
+        middleware: ['logInput']
+    },
+
+    'nameSet': {
+        activeIntents: ['welcome','name'],
         middleware: ['logInput']
     },
 
