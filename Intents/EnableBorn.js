725b9e3cc73e435821f83114f3bffff2a21e8f6c
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-02-20 15:22:31 -0500

    corrected variable name

--- Intents/EnableBorn.js
+++ Intents/EnableBorn.js
@@ -3,9 +3,9 @@
 var Ayva = require('ayva')
 var StateManager = Ayva.StateManager
 
-var Age = function(Context){
+var EnableBorn = function(Context){
     StateManager.setState('bornEnabled', Context)
     Context.assistant.say("Command enabled. Please try asking me again where I was born.").finish()
 }
 
-module.exports = Age;
+module.exports = EnableBorn;
