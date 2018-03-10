13c2388ce7d1f320ef870bf09a9f0666272e3d06
gberk <gavin.berkowitz.bu@gmail.com>
2018-03-10 17:10:45 -0500

    Manual merge speechModelFolder branch

---
+++ SpeechModel/Intents/Born.js
@@ -0,0 +1,13 @@
+var Born = {
+    name: 'Born',
+    utterances: [
+        "where were you born",
+        "tell me where you were born",
+        "where are you from",
+        "tell me where you are from"
+    ],
+    events:[],
+    isList: false
+}
+
+module.exports = Born;
---
+++ SpeechModel/Intents/EnableBorn.js
@@ -0,0 +1,19 @@
+// EnableBorn Intent
+var EnableBorn = {
+    name: 'EnableBorn',
+    utterances: [
+        "enable where were you born intent",
+        "allow me to ask Ayva where she was born",
+        "allow me to ask you where you were born",
+        "allow where was ayva born intent",
+        "enable the born intent",
+        "enable born intent",
+        "enable the intent",
+        "enable intent",
+        "enable"
+    ],
+    events:[],
+    isList: false
+}
+
+module.exports = EnableBorn;
