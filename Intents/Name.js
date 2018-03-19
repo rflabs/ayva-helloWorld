df1ecaf494821684bc4a4747267d5b60e552f46e
gberk <gavin.berkowitz.bu@gmail.com>
2018-03-19 15:35:43 -0400

    stash

--- Intents/Name.js
+++ Intents/Name.js
@@ -6,7 +6,7 @@ var StateManager = Ayva.StateManager
 var Name = function(Context){
     let name = Context.args.firstName
     StateManager.setState('bornDisabled', Context)
-    Context.assistant.say("Hello " + name + ". I can remember your name using my speech model file. My states file helps track what commands are currently active. Try asking me where I was born.").finish()
+    Context.assistant.say(`Hello ${name}! Congratulations on completing the tutorial, you are now ready to begin building using the Ayva Framework!`).finish()
 }
 
 module.exports = Name;
--- states.js
+++ states.js
@@ -3,18 +3,7 @@ var States = {
     'welcome': {
         activeIntents: ['Welcome','Name'],
         middleware: ['logInput']
-    },
-
-    'bornDisabled': {
-        activeIntents: ['Welcome','EnableBorn'],
-        middleware: ['logInput']
-    },
-    
-    'bornEnabled': {
-        activeIntents: ['Welcome','Born'],
-        middleware: ['logInput']
     }
-
 }
 
 module.exports = States
